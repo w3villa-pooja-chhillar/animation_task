9eeaa655f48a4d3994442bf90bc23ec782106fab
Pooja Chhillar <pooja.chhillar@w3villa.com>
2023-08-12 17:04:38 +0530

    add animation effect

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 10,
+      loop: true,
+      nav: true,
+      autoplay: true, // Enable autoplay
+      autoplayTimeout: 3000
+    });
+  });
+
